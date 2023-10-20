6f4e926a403744e4bad9276eff57bec21f0f1d6f
xWIREHEADx <mattadamosart@gmail.com>
2023-10-20 10:07:06 -0400

    User fixed

--- MidTerm.py
+++ MidTerm.py
@@ -8,28 +8,34 @@ class User:
         return f"Welcome to the computer store {self.username}"
 
 class Product:
-    def __init__(self, price, items, discount):
+    def __init__(self, price, items, quantity, discount):
         self.price = price
         self.items = items
+        self.quantity = quantity
         self.discount = discount
 
     def total(self):
         if self.discount == "Yes" or "YES" or "yes" or "y":
-            total = (items * price) - discount
+            total = (self.quantity * self.price) - self.discount
         else:
-            total = (items * price)
+            total = (self.quantity * self.price)
         return total
-
-
 
 #Input
 
 User_name = input("What is your username? ")
 
+userItem = input("What are you trying to buy? ")
+userPrice = input("How much is the item? ")
+userQuantity = input("How many are you trying to buy? ")
+userDiscount = input("Would you like to apply a discount? ")
+
 #Objects
 
 user = User(User_name)
+product = Product(userItem, userPrice, userQuantity, userDiscount)
 
 #Output
 
 print(user)
+print(product)
